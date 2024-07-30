01592f2a027bec92f82e66ca8376b433fb476387
Passionate Flutter developer <129858140+RomanDevelop@users.noreply.github.com>
2024-07-30 10:50:18 +0200

    signup view model done

--- lib/features/auth/viewmodel/auth_viewmodel.dart
+++ lib/features/auth/viewmodel/auth_viewmodel.dart
@@ -1,8 +1,36 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:client/core/model/user_model.dart';
+import 'package:client/features/auth/repositories/auth_remote_repository.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-class AuthViewmodel extends Notifier {
+part 'auth_viewmodel.g.dart';
+
+@riverpod
+class AuthViewmodel extends _$AuthViewmodel {
+  final AuthRemoteRepository _authRemoteRepository = AuthRemoteRepository();
   @override
-  build() {
-    throw UnimplementedError();
+  AsyncValue<UserModel>? build() {
+    return null;
+  }
+
+  Future<void> sighUpUser({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    state = const AsyncValue.loading();
+    final res = await _authRemoteRepository.signup(
+      name: name,
+      email: email,
+      password: password,
+    );
+    final val = switch (res) {
+      Left(value: final l) => state = AsyncValue.error(
+          l.message,
+          StackTrace.current,
+        ),
+      Right(value: final r) => state = AsyncValue.data(r),
+    };
+    print(val);
   }
 }
