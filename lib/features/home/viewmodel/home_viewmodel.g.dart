03df3f23981ec0f12c0c57a0fce8b525f2c1cd3c
Passionate Flutter developer <129858140+RomanDevelop@users.noreply.github.com>
2024-08-26 14:57:01 +0200

    songs page initial, just audio

--- lib/features/home/viewmodel/home_viewmodel.g.dart
+++ lib/features/home/viewmodel/home_viewmodel.g.dart
@@ -6,6 +6,20 @@ part of 'home_viewmodel.dart';
 // RiverpodGenerator
 // **************************************************************************
 
+String _$getAllSongsHash() => r'a53a3d396d1fb56df51c87f5b8268be0061ba2d3';
+
+/// See also [getAllSongs].
+@ProviderFor(getAllSongs)
+final getAllSongsProvider = AutoDisposeFutureProvider<List<SongModel>>.internal(
+  getAllSongs,
+  name: r'getAllSongsProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$getAllSongsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef GetAllSongsRef = AutoDisposeFutureProviderRef<List<SongModel>>;
 String _$homeViewModelHash() => r'f03e5bb133df4262ecc2d1550c008de0563bb505';
 
 /// See also [HomeViewModel].
